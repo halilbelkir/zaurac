047528f8a36dedbca4fed31edbe24fcd3ee5f0fe
Halil İbrahim BELKIR <ibrahim.belkir@tardigrad.com.tr>
2021-10-05 00:05:34 +0300

    Halil İbrahim BELKIR tarafindan yapildi

--- resources/views/components/front/front.blade.php
+++ resources/views/components/front/front.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="default-src * gap:; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *; img-src * data: blob: android-webview-video-poster:; style-src * 'unsafe-inline';">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="keywords" content="{{$keywords}}" />
     <meta name="description" content="{{$description}}" />
